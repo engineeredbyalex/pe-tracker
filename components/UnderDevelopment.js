cd96cccd74191a691421d5afcc8ecf10d460af65
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-03-20 00:14:10 +0200

    20.03 rev-6

--- components/UnderDevelopment.js
+++ components/UnderDevelopment.js
@@ -2,7 +2,7 @@ export default function UnderDevelopment() {
     return (
           <div className="flex w-full items-center justify-center">
             <h4 className="bg-purple-200 text-purple-700 font-normal text-center py-3 ">
-               <span className="uppercase font-bold">Under development.</span> <br/> For any suggestions or bug reports, feel free to message me on Reddit or email at dev.lazarescu.alexandru@gmail.com.
+               <span className="uppercase font-bold">Under development.</span> <br/> For any suggestions or bug reports, feel free to message me on Reddit or email at xzsplit69@gmail.com.
             </h4>
         </div>
     )
