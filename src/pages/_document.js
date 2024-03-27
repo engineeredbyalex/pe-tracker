18b23484fd8237d70e64f1544b6225967b086a1e
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-03-27 23:59:32 +0200

    27.01 rev-2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head >
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
-        <meta name="theme-color" content="#000" />
+        <link rel="favicon" href="/favicon.ico"></link>
       </Head>
       <body>
         <Main />
